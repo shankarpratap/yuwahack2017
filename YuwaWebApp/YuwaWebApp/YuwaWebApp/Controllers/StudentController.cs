7ec19b822bcf1a884038a5e5095b0daeff0d2917
Madhura Phadke <maphadke@microsoft.com>
2017-07-25 15:49:33 -0700

    student controller

---
+++ YuwaWebApp/YuwaWebApp/YuwaWebApp/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using YuwaWebApp.Models;
+
+namespace YuwaWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // POST: /Account/AddNewStudent
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddNewStudent(StudentModel student)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+}
